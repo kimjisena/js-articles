40676fd85d96d56929dcc8a50991a2115dc060c6
Kim Jisena <pjohmaths@gmail.com>
2022-08-22 18:18:25 +0300

    add article code

--- index.ts
+++ index.ts
@@ -1,7 +1,45 @@
 type List = {
-  value?: any
+  value?: any,
+  __proto__?: List | null,
 }
 
 function getEmptyList (): List {
   return Object.prototype;
+}
+
+function makeList (element: any, list: List): List {
+  list = Object.create(list);
+  list.value = element; 
+  return list;
+}
+
+function first (list: List): List {
+  return list.value;
+}
+
+function rest (list: List): List {
+  return list.__proto__;
+}
+
+function isEmpty (list: List): boolean {
+  return list.__proto__ === null;
+}
+
+function replaceFirst (element: any, list: List): List {
+  list.value = element;
+  return list;
+}
+
+function replaceRest (rest: List, list: List): List {
+  let value = list.value;
+  list = Object.create(rest);
+  list.value = value;
+  return list;
+}
+
+function printList (list: List): void {
+  while(!isEmpty(list)) {
+    console.log(list.value);
+    list = rest(list);
+  }
 }
